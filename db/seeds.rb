a0c08ae5e4c1ea974b4a24ae91d9f1f5f923802d
Jimmy Charpagne <jimmy.charpagne@gmail.com>
2019-01-31 05:45:33 +0100

    Push final des enfers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do
+  user = User.create!(name: Faker::SiliconValley.character)
+ end
+ 
+ 10.times do
+  post = Post.create!(url: Faker::SiliconValley.url, user_id: rand(1..10))
+ end
+ 
+ 10.times do
+  comment = Comment.create!(content: Faker::SiliconValley.quote, user_id: rand(1..5), post_id: rand(1..10))
+ end
